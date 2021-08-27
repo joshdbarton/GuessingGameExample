b2d0c66c386b922cd286154a36847fc4c84b5060
Josh Barton <joshdbarton@gmail.com>
2021-08-27 11:54:00 -0500

    first push

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the secret number!");
+            Console.Write("Your Guess: ");
+            var input = Console.ReadLine();
+            Console.WriteLine($"You guessed: {input}");
+        }
+    }
+}
