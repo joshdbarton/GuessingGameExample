4b2629dc083583ebef51afb7da757efcfd6165a8
Josh Barton <joshdbarton@gmail.com>
2021-08-27 12:11:01 -0500

    adding phase 7

--- Program.cs
+++ Program.cs
@@ -6,9 +6,16 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("difficulty level? 1 -easy, 2 - medium, 3 -hard");
+            var difficulty = int.Parse(Console.ReadLine());
+
             int secretNumber = new Random().Next(1, 101);
 
-            for (int i = 4; i > 0; i--)
+            //represents number of tries by difficult level
+            var difficultiesTries = new int[] { 8, 6, 4 };
+
+            for (int i = difficultiesTries[difficulty - 1]; i > 0; i--)
             {
                 Console.WriteLine("Guess the secret number!");
                 Console.Write($"Your Guess (Guesses Left: {i}): ");
