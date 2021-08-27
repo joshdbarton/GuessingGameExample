06deb540df5e41cf929b2fd5f6646951ffaac74f
Josh Barton <joshdbarton@gmail.com>
2021-08-27 12:04:20 -0500

    adding phase 2

--- Program.cs
+++ Program.cs
@@ -6,10 +6,29 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            int secretNumber = 42;
+
             Console.WriteLine("Guess the secret number!");
             Console.Write("Your Guess: ");
-            var input = Console.ReadLine();
-            Console.WriteLine($"You guessed: {input}");
+
+            try
+            {
+                var input = int.Parse(Console.ReadLine());
+                if (secretNumber == input)
+                {
+                    Console.WriteLine("correct!");
+                }
+                else
+                {
+                    Console.WriteLine("wrong");
+                };
+            }
+            catch
+            {
+                Console.WriteLine("Integers only please!");
+
+            }
+
         }
     }
 }
