6f6a79a417d8ff936a2d4f48a00a7003760ca43e
Josh Barton <joshdbarton@gmail.com>
2021-11-16 15:50:59 -0600

    better variable names

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ namespace GuessingGame
 
             Console.WriteLine("difficulty level? 1 -easy, 2 - medium, 3 -hard, 4 - cheater");
             var difficulty = int.Parse(Console.ReadLine());
-            int i = new int[] { 8, 6, 4, 1}[difficulty - 1];
+            int numberOfGuesses = new int[] { 8, 6, 4, 1}[difficulty - 1];
 
             int secretNumber = new Random().Next(1, 101);
             
-            while (difficulty == 4 || i > 0)
+            while (difficulty == 4 || numberOfGuesses > 0)
             {
-                string guessesLeft = difficulty == 4 ? "INFINITE!" : i.ToString();
+                string guessesLeft = difficulty == 4 ? "INFINITE!" : numberOfGuesses.ToString();
                 Console.WriteLine("Guess the secret number!");
                 Console.Write($"Your Guess (Guesses Left: {guessesLeft}): ");
                 var input = int.Parse(Console.ReadLine());
@@ -33,7 +33,7 @@ namespace GuessingGame
                 {
                     Console.WriteLine("too High!");
                 };
-                i--;
+                numberOfGuesses--;
             }
            
         }
