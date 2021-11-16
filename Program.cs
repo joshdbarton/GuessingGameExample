8c0089c6df58a447f972c9b5653aa3d376c8078f
Josh Barton <joshdbarton@gmail.com>
2021-11-16 15:43:14 -0600

    adding a difficulty

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GuessingGame
 {
@@ -7,15 +8,23 @@ namespace GuessingGame
         static void Main(string[] args)
         {
 
-            Console.WriteLine("difficulty level? 1 -easy, 2 - medium, 3 -hard");
+            Console.WriteLine("difficulty level? 1 -easy, 2 - medium, 3 -hard, 4 -hardest");
             var difficulty = int.Parse(Console.ReadLine());
 
             int secretNumber = new Random().Next(1, 101);
 
             //represents number of tries by difficult level
-            var difficultiesTries = new int[] { 8, 6, 4 };
+            List<int> difficultiesTries = new List<int>
+             { 
+                 8, 
+                 6, 
+                 4, 
+                 2 
+             };
 
-            for (int i = difficultiesTries[difficulty - 1]; i > 0; i--)
+            int numOfTries = difficultiesTries[difficulty - 1];
+
+            for (int i = numOfTries; i > 0; i--)
             {
                 Console.WriteLine("Guess the secret number!");
                 Console.Write($"Your Guess (Guesses Left: {i}): ");
